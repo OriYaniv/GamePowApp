6092e5c240cb86e48ec14d9aec1a7d16eaaf6979
OriYaniv <oriyaniv21@gmail.com>
2021-01-31 15:57:27 +0200

    update

---
+++ app/src/main/java/com/example/gamepowapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.gamepowapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView textViewTopTime;
+    private Button buttonLevel1, buttonLevel2, buttonLevel3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initUI();
+        initListeners();
+    }
+
+    private void initUI() {
+        textViewTopTime = findViewById(R.id.textViewTopTime);
+        buttonLevel1 = findViewById(R.id.buttonLevel1);
+        buttonLevel2 = findViewById(R.id.buttonLevel2);
+        buttonLevel3 = findViewById(R.id.buttonLevel3);
+    }
+
+    private void initListeners() {
+        buttonLevel1.setOnClickListener(this);
+        buttonLevel2.setOnClickListener(this);
+        buttonLevel3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        if (v.getId() == R.id.buttonLevel1) {
+            intent.putExtra("level", 1);
+            startActivity(intent);
+        }
+        if (v.getId() == R.id.buttonLevel2) {
+            intent.putExtra("level", 2);
+            startActivity(intent);
+        }
+        if (v.getId() == R.id.buttonLevel3) {
+            intent.putExtra("level", 3);
+            startActivity(intent);
+        }
+    }
+
+}
